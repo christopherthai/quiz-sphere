d0673f2d3be473fc7f3801ac35d30ec90117a3cf
Christopher Thai <christopherthai.nyli@gmail.com>
2024-04-12 21:41:29 -0400

    chore: Add getter, setter and class method to user.py

--- lib/models/user.py
+++ lib/models/user.py
@@ -1,3 +1,6 @@
+from models.__init__ import CURSOR, CONN
+
+
 class User:
 
     all = {}
@@ -7,6 +10,10 @@ class User:
         self.username = username
         self.is_admin = is_admin
         type(self).all[username] = self
+
+    # Method that returns representation of the object
+    def __repr__(self):
+        return f"<User {self.id}: {self.username}" + f"Is_admin: {self.is_admin}>"
 
     # Property method that returns the username
     @property
@@ -31,3 +38,100 @@ class User:
         if value not in (0, 1):
             raise ValueError("Invalid value for is_admin")
         self._is_admin = value
+
+    @classmethod
+    def create_table(cls):
+        """Create a new table to persist the attributes of the User instance"""
+        sql = """
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            is_admin INTEGER NOT NULL
+        )
+        """
+
+        CURSOR.execute(sql)  # Execute the SQL statement
+        CONN.commit()  # Commit the changes to the database
+
+    @classmethod
+    def drop_table(cls):
+        """Drop the table that persists the attributes of the User instance"""
+        sql = "DROP TABLE IF EXISTS users"
+        CURSOR.execute(sql)  # Execute the SQL statement
+        CONN.commit()  # Commit the changes to the database
+
+    def save(self):
+        """Insert a new record into the Users table with the attributes of the User instance"""
+        sql = """
+        INSERT INTO Users (username, is_admin) VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.username, self.is_admin))  # Execute the SQL statement
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid  # Get the id of the last inserted row
+        type(self).all[
+            self.username
+        ] = self  # Add the User instance to the all dictionary
+
+    def update(self):
+        """Update the record in the Users table with the attributes of the User instance"""
+        sql = """
+        UPDATE Users SET username = ?, is_admin = ? WHERE id = ?
+        """
+        CURSOR.execute(
+            sql, (self.username, self.is_admin, self.id)
+        )  # Execute the SQL statement
+        CONN.commit()  # Commit the changes to the database
+
+    def delete(self):
+        """Delete the record in the Users table with the attributes of the User instance"""
+        sql = """
+        DELETE FROM Users WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.id,))  # Execute the SQL statement
+        CONN.commit()  # Commit the changes to the database
+
+    @classmethod
+    def instance_from_db(cls, row):
+        """Return a User object having the attributes value from the table row"""
+
+        # Checking the dictionary for the existing instance using the row's primary key
+        user = cls.all.get(row[0])  # Get the User instance from the all dictionary
+        if user:
+            # Making sure the instance has the latest data
+            user.username = row[1]
+            user.is_admin = row[2]
+        else:
+            # Create a new instance of the User class
+            user = cls(row[0], row[1], row[2])
+        return user
+
+    @classmethod
+    def get_all(cls):
+        """Return all records from the Users table"""
+        sql = """
+        SELECT * FROM Users
+        """
+        CURSOR.execute(sql)  # Execute the SQL statement
+        rows = CURSOR.fetchall()  # Returns all rows
+        return [cls(*row) for row in rows]  # Create a User instance for each row
+
+    @classmethod
+    def find_by_id(cls, id):
+        """Return the record from the Users table with the given id"""
+        sql = """
+        SELECT * FROM Users WHERE id = ?
+        """
+        CURSOR.execute(sql, (id,))  # Execute the SQL statement
+        row = CURSOR.fetchone()  # Returns the first row
+        return cls(*row) if row else None  # Create a User instance if row is not None
+
+    @classmethod
+    def find_by_username(cls, username):
+        """Return the record from the Users table with the given username"""
+        sql = """
+        SELECT * FROM Users WHERE username = ?
+        """
+        CURSOR.execute(sql, (username,))  # Execute the SQL statement
+        row = CURSOR.fetchone()  # Returns the first row
+        return cls(*row) if row else None  # Create a User instance if row is not None
